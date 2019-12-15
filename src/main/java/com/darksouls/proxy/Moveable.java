166056a31e7377f56a2ad153f9938e9264591407
DarkSouls_lzy <1160827860@qq.com>
2019-12-15 11:40:30 +0800

    delect other branch
    删除其他分支的内容

--- src/main/java/com/darksouls/proxy/Moveable.java
+++
@@ -1,6 +0,0 @@
-package com.darksouls.proxy;
-
-public interface Moveable {
-     void move();
-
-}
--- src/main/java/com/darksouls/proxy/diyproxy/InvocationHandler.java
+++
@@ -1,7 +0,0 @@
-package com.darksouls.proxy.diyproxy;
-
-import java.lang.reflect.Method;
-
-public interface InvocationHandler {
-    public  void invoke(Object o, Method m);
-}
